d2b9d49b3ae9d01a1ac3056b1b484ee047fe162c
michelle <113802263+michelletrn@users.noreply.github.com>
2022-11-25 18:50:22 -0500

    saving working code, this code generates a password that meets the requirements into console.log

--- script.js
+++ script.js
@@ -1,98 +1,65 @@
 var generateBtn = document.querySelector("#generate");
 
-var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+var lowercase = 'abcdefghijklmnopqrstuvwxyz'; 
 var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var special = '!@#$%^&*';
 var numbers = '0123456789';
-
-
-
-function generatePassword() {
-  let passChar = ' ';
-  
-  let passLength = parseInt(
-    prompt("How long would you like your password? Please choose a number between 8 and 128.")
-    );
-
-  if (passLength < 8 || passLength > 128) {
-    alert("Chosen length is outside of given range, please try again")
-  } else {
-    
-  }
-
-
-
-}
-
+var possChars = ''; // variable to hold all the possible characters according to the character types the user chose
+var requiredChars = ''; // variable to hold a random character from each character type to ensure that at least one character from each desired list is in the final password
+var fillMissing = ''; // variable to hold the remaining #of missing characters
 
 generatePassword();
 
+function generatePassword() {
+  let passLength = parseInt(
+    prompt("How long would you like your password? Please choose a number between 8 and 128.")
+  );
 
-
-
-
-
-
-
-
-
-
-
-
-function randomLowercaseChars() {
-  let randomLowercase = ' ';
-  let lowercaseLength = lowercase.length;
-  for (let i = 0; i < lowercaseLength; i++) {
-    randomLowercase += lowercase.charAt(Math.floor(Math.random() * lowercaseLength))
+  if (isNaN(passLength) || passLength < 8 || passLength > 128) {
+    alert("Chosen length is outside of given range, please try again")
+    generatePassword();
+  } else {
+    alert("Your password will be " + passLength + " characters long.");
   }
-    return randomLowercase; 
-}
-console.log(randomLowercaseChars());
 
-function randomUppercaseChars() {
-  let randomUppercase = ' ';
-  let uppercaseLength = uppercase.length;
-  for (let i = 0; i < uppercaseLength; i++) {
-    randomUppercase += uppercase.charAt(Math.floor(Math.random() * uppercaseLength))
+  //user
+  var includeLowercase = confirm("Would you like lowercase characters to be in your password?");
+  var includeUppercase = confirm("Would you like uppercase characters to be in your password?");
+  var includeSpecial = confirm("Would you like special characters to be in your password?");
+  var includeNumbers = confirm("Would you like numbers to be in your password?");
+
+  if (!includeLowercase && !includeUppercase && !includeSpecial && !includeNumbers) {
+    alert("Please include at least one character type");
   }
-    return randomUppercase; 
-}
-console.log(randomUppercaseChars());
 
-function randomSpecialChars() {
-  let randomSpecial = " ";
-  let specialLength = special.length;
-  for (let i = 0; i < specialLength; i++) {
-    randomSpecial += special.charAt(Math.floor(Math.random() * specialLength))
+  if (includeLowercase) {
+    possChars += lowercase;
+    requiredChars += lowercase.charAt(Math.floor(Math.random() * lowercase.length));
   }
-  return randomSpecial;
-}
-console.log(randomSpecialChars());
-
-function randomNumberChars() {
-  let randomNumbers = ' ';
-  let numbersLength = numbers.length;
-  for (let i = 0; i < numbersLength; i++) {
-    randomNumbers += numbers.charAt(Math.floor(Math.random() * numbersLength))
+  if (includeUppercase) {
+    possChars += uppercase;
+    requiredChars += uppercase.charAt(Math.floor(Math.random() * uppercase.length));
   }
-    return randomNumbers;
+  if (includeSpecial) {
+    possChars += special;
+    requiredChars += special.charAt(Math.floor(Math.random() * special.length));
+  }
+  if (includeNumbers) {
+    possChars += numbers;
+    requiredChars += numbers.charAt(Math.floor(Math.random() * numbers.length));
+  }
+
+  var missingChars = (passLength) - (requiredChars.length);
+  for (let i = 0; i < missingChars; i++) {
+    fillMissing += possChars.charAt(Math.floor(Math.random() * possChars.length));
+  }
+
+  finalPass = requiredChars + fillMissing;
+  console.log(finalPass);
+  
+
 }
-console.log(randomNumberChars());
 
-
-
-
-
-
-
-
-
-
-
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// //
 
 // // Write password to the #password input
 // function writePassword() {
